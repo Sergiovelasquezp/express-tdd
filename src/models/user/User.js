4a89eedd9d1d11b503643e1311fdc449ae37feec
Sergio Velasquez <sergiovelasquezp@gmail.com>
2022-07-04 00:33:44 -0400

    Added Database Configuration And User Model

---
+++ src/models/user/User.js
@@ -0,0 +1,16 @@
+const {DataTypes} = require('sequelize');
+const sequelize = require('../../config/database');
+
+const User = sequelize.define('User', {
+  username: {
+    type: DataTypes.STRING
+  },
+  email: {
+    type: DataTypes.STRING
+  },
+  password: {
+    type: DataTypes.STRING
+  }
+});
+
+module.exports = User;
